f2f95d3ba61bb6e1312cea7ad9798ca55d6d6bf1
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2016-09-24 14:56:04 -0700

    Refactor resource definition loading into helper.

---
+++ test/lib/get-resource-definition.js
@@ -0,0 +1,61 @@
+var assert = require('assert')
+var getResourceDefinition = require('../../lib/get-resource-definition')
+var SPEC = {
+  definitions: {
+    Valid: {
+      properties: {
+        test_definition: true,
+      },
+    },
+    Empty: {
+    },
+  },
+}
+
+module.exports = {
+  'getResourceDefinition returns the definition': function () {
+    var subject = getResourceDefinition(SPEC, 'Valid')
+
+    assert.equal(subject.properties.test_definition, true)
+  },
+  'getResourceDefinition fills in properties, if not specified': function () {
+    var subject = getResourceDefinition(SPEC, 'Empty')
+
+    assert(subject.properties && typeof subject.properties === 'object')
+  },
+  'getResourceDefinition fills in properties.attributes, if not specified': function () {
+    var subject = getResourceDefinition(SPEC, 'Empty')
+
+    assert(subject.properties.attributes && typeof subject.properties.attributes === 'object')
+  },
+  'getResourceDefinition fills in properties.relationships, if not specified': function () {
+    var subject = getResourceDefinition(SPEC, 'Empty')
+
+    assert(subject.properties.relationships && typeof subject.properties.relationships === 'object')
+  },
+  'getResourceDefinition fills in properties non-destructively, if not specified': function () {
+    var subject = getResourceDefinition(SPEC, 'Empty')
+
+    assert.equal(SPEC.definitions.Empty.properties, null)
+  },
+  'getResourceDefinition throws if spec is null': function () {
+    assert.throws(function () {
+      getResourceDefinition(null, 'Valid')
+    })
+  },
+  'getResourceDefinition throws if spec is not an object': function () {
+    assert.throws(function () {
+      getResourceDefinition(42)
+    })
+  },
+  'getResourceDefinition throws if spec has no definitions': function () {
+    assert.throws(function () {
+      getResourceDefinition({}, 'Valid')
+    })
+  },
+  'getResourceDefinition throws if name is not defined': function () {
+    assert.throws(function () {
+      getResourceDefinition(SPEC, 'DoesNotExist')
+    })
+  },
+}

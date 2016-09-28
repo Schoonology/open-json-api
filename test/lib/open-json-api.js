b5a7fa06b53fd5db779617d0c10cc9fffb4b7dc7
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2016-09-27 21:11:19 -0700

    Rename 'serialize' to 'normalize'.

--- test/lib/open-json-api.js
+++ test/lib/open-json-api.js
@@ -2,7 +2,7 @@ var assert = require('assert')
 var oja = require('../../lib/open-json-api')
 
 module.exports = {
-  'oja exports serialize': function () {
-    assert.equal(typeof oja.serialize, 'function')
+  'oja exports normalize': function () {
+    assert.equal(typeof oja.normalize, 'function')
   },
 }

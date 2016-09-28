ae1de8d8448a15ed42b3cc18ce431ba9990f2851
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2016-09-27 21:42:11 -0700

    Add top-level 'flatten' function.
    This function explicitly flattens a JSONAPI Resource into a POJO, mixing
    attributes and relationships (i.e. all 'fields') into the outer namespace.

---
+++ test/lib/flatten.js
@@ -0,0 +1,78 @@
+var assert = require('assert')
+var flatten = require('../../lib/flatten')
+
+function generateTestResource() {
+  return {
+    type: 'widgets',
+    id: '777',
+    attributes: {
+      name: 'Test Widget',
+    },
+    relationships: {
+      company: {
+        type: 'companies',
+        id: '42',
+      },
+    },
+  }
+}
+
+module.exports = {
+  'flatten collapses attributes and relationships to top-level fields': function () {
+    var original = generateTestResource()
+
+    var subject = flatten(original)
+
+    assert.deepEqual(subject, {
+      id: '777',
+      name: 'Test Widget',
+      company: { id: '42' },
+    })
+  },
+  'flatten ignores missing attributes': function () {
+    var original = generateTestResource()
+
+    delete original.attributes
+
+    var subject = flatten(original)
+
+    assert.deepEqual(subject, {
+      id: '777',
+      company: { id: '42' },
+    })
+  },
+  'flatten ignores missing relationships': function () {
+    var original = generateTestResource()
+
+    delete original.relationships
+
+    var subject = flatten(original)
+
+    assert.deepEqual(subject, {
+      id: '777',
+      name: 'Test Widget',
+    })
+  },
+  'flatten ignores invalid relationships': function () {
+    var original = generateTestResource()
+
+    delete original.relationships.company.id
+
+    var subject = flatten(original)
+
+    assert.deepEqual(subject, {
+      id: '777',
+      name: 'Test Widget',
+    })
+  },
+  'flatten ignores empty objects': function () {
+    var subject = flatten({})
+
+    assert.deepEqual(subject, {})
+  },
+  'flatten returns null if the data is null': function () {
+    var subject = flatten(null)
+
+    assert.equal(subject, null)
+  },
+}
